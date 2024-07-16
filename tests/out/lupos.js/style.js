5c5200c7f1afe39b20d66766558c554869348a9d
pucelle <pucelle@163.com>
2024-07-16 12:18:47 +0800

    Test result update

--- tests/out/lupos.js/style.js
+++ tests/out/lupos.js/style.js
@@ -1,39 +1,15 @@
 import { Component, addGlobalStyle, css } from '@pucelle/lupos.js';
 class TestNormalStyle extends Component {
-    static style = css `
-		.a{
-			.b{
-				color: red;
-			}
-		}
-	`;
+    static style = css `.a{}.a .b{color:red;}`;
 }
 class TestDynamicStyle extends Component {
     static style() {
-        return css `
-			.a{
-				.b{
-					color: red;
-				}
-			}
-		`;
+        return css `.a{}.a .b{color:red;}`;
     }
 }
 class TestDynamicStyleWithValues extends Component {
     static style() {
-        return css `
-			.a{
-				color: ${"red"};
-
-				.b{
-					color: ${"green"};
-				}
-			}
-		`;
+        return css `.a{color:${"red"};}.a .b{color:${"green"};}`;
     }
 }
-addGlobalStyle(css `.a{
-	.b{
-		color: red;
-	}
-}`);
+addGlobalStyle(css `.a{}.a .b{color:red;}`);
