615934446de653380d3c681f35590299ddcf8073
Freeman Chen <pucelle@163.com>
2025-11-10 11:02:05 +0800

    Add a `promisify`

--- web/src/utils/promise.ts
+++ web/src/utils/promise.ts
@@ -18,3 +18,14 @@ export function promiseWithResolves<T = void>(): {
 		reject: reject!,
 	}
 }
+
+
+/** 
+ * Normally we would suggest you use callback, and when needed,
+ * you may transform callback to promise by this.
+ */
+export function promisify(fn: (callback: () => void) => void, scope: any = null) {
+	return new Promise<void>((resolve) => {
+		fn.call(scope, resolve)
+	})
+}
