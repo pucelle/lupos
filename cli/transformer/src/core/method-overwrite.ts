0352e4fe0347ceeccf49206eba1d1741b3802001
Freeman Chen <pucelle@163.com>
2025-11-27 12:39:48 +0800

    Fix type errors

--- cli/transformer/src/core/method-overwrite.ts
+++ cli/transformer/src/core/method-overwrite.ts
@@ -28,10 +28,10 @@ export class MethodOverwrite {
 		this.name = name
 
 		if (name === 'constructor') {
-			this.rawNode = helper.class.getConstructor(classNode, false) ?? null
+			this.rawNode = helper.class.getConstructorDeclaration(classNode, false) ?? null
 		}
 		else {
-			this.rawNode = helper.class.getMethod(classNode, name, false) ?? null
+			this.rawNode = helper.class.getMethodDeclaration(classNode, name, false) ?? null
 		}
 
 		if (this.rawNode) {
